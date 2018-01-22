ffcedf34933e4f6687dd108180c86814bf1d0a2b
billie99 <giandomenicoiameo99@gmail.com>
2018-01-23 00:12:42 +0100

    aggiunti file

---
+++ Class.java
@@ -0,0 +1,14 @@
+import scuola.index;
+
+public class Class 
+{
+
+	public static void main(String[] args)
+	{		
+		index s = new index();
+		s.set_alunni(5);
+		
+		System.out.println( s.get_alunni() );
+	}
+
+}
---
+++ scuola/index.java
@@ -0,0 +1,28 @@
+package scuola;
+
+public class index 
+{
+	private int voti;
+	private int alunni;
+
+public void set_alunni(int x)
+{
+	this.alunni = x;
+}
+
+public void set_voti(int y)
+{
+	this.voti = y;
+}
+
+public int get_voti()
+{
+	return this.voti;
+}
+
+public int get_alunni()
+{
+	return this.alunni;
+}
+
+};
